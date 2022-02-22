098e0cb3e981e806b5b40bdb86a2d1d8754fd0d7
Shahzaneer Ahmed <shahism01@gmail.com>
2022-02-22 20:47:12 +0500

    linear search added

---
+++ Lab Task Code/Arrays/Search/linearSearch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+//* linear Search --> ismain aik aik kr k har element ko check krte hain
+//* jahan mil jayegi key wahan return krte hain!
+
+
+int linearSearch(int  arr [], int n, int key){
+
+    for(int i=0; i<=n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+    } 
+
+
+int main(){
+    int array [10] = {10,3,47,5,78,89,5,9,76,4};
+    cout<<linearSearch(array,10,78);
+
+return 0;
+}
