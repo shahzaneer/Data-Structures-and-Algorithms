7236df7bd803808ba803db99f46ae8ae1064b06b
Shahzaneer Ahmed <shahism01@gmail.com>
2022-03-06 00:43:36 +0500

    adt array 1.0

--- Codes/DS and Algo/Arrays/ArrayADT.cpp
+++ Codes/DS and Algo/Arrays/ArrayADT.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 // implementing array as An ADT through DMA.
 struct myArray{
+    // srff in teen attributes se humne aik array bnaali hai!
     int total_size;
     int used_Size;
     int *p;
+};
 
-    void createArray(myArray *ptr , int tSize , int uSize){
+void createArray(myArray *ptr , int tSize , int uSize){
 
         // Structure k object ka address hoga is pointer main tou ager isko indirection 
         // symbol lgaaden tou yeh bn jayega object.
@@ -19,7 +21,9 @@ struct myArray{
         // yani ager kisi object ka pointer ho tou usse indirection operator laga kr object bananay ki bajaye -> use krskte hain.
         ptr -> total_size = tSize;
         ptr -> used_Size = uSize;
-        ptr -> p = new int [tSize];
+        ptr -> p = new int [tSize]; 
+        //yeh dma se kyun banaya hai ? q k ager yeh sma se banaya jata tou function k khtm hone per memory urraadi jaati jbke heap main 
+        // jb tk program khtm na hojaye tb tk rehti hai memory isliay hamne isse banaaya hai heap main
 
         cout<<"Your Array has been created!"<<endl;
     }
@@ -28,8 +32,10 @@ struct myArray{
 
         for(int i=0;i<ptr->used_Size;i++){
             cout<<"Enter Value at"<< i << "index "<<endl;
+            // different ways for setting values
             // cin>>((*(ptr)).p)[i];
-            cin>>ptr->p[i];
+            // cin>>ptr->p[i];
+            cin>>*(ptr->p+i);
         }
     }
 
@@ -49,15 +55,14 @@ struct myArray{
         }
     }
 
-};
 
 int main(){
 
     myArray marks; //object creation of structure myArray
-    marks.createArray(&marks,10,5);
-    marks.setValues(&marks);
-    marks.showValues(&marks);
-    marks.showAddresses(&marks);
+    createArray(&marks,10,5);
+    setValues(&marks);
+    showValues(&marks);
+    showAddresses(&marks);
 
     // cout<<*(marks.p)<<endl; //it contains the first element
     delete []marks.p;
