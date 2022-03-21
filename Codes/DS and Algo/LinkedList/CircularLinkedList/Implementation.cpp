9b9d535ad8a719525420037d4e8b06d2ff27ad1a
Shahzaneer Ahmed <shahism01@gmail.com>
2022-03-21 18:38:06 +0500

    circular linkedlist implementation

--- Codes/DS and Algo/LinkedList/CircularLinkedList/Implementation.cpp
+++ Codes/DS and Algo/LinkedList/CircularLinkedList/Implementation.cpp
@@ -5,7 +5,24 @@ struct Node{
     int data;
     Node *next;
 };
+
+// Count the linkedlist members
+int countList(Node *last){
+    if(last == NULL) return 0;
+    
+    int i = 1; //circular k case main pehle lelengay 1
+    Node *p = last->next;
+
+    while(p->next!=last->next){
+        p = p->next;
+        i++;
+    }
+
+    return i;
+}
+
 // Inserting
+
 Node* insertAtEnd(Node *last, int value){
     Node *newNode = new Node;
     if(last == NULL){
@@ -68,7 +85,7 @@ void insertAtPosition(Node *last ,int position,int value){
 }
 void insertAfterPosition(Node *last , int position , int value){
     if(last == NULL){
-        cout<<"";
+        cout<<"linkedlist is empty";
     }
     Node *newNode = new Node;
     Node *p = last;
@@ -86,10 +103,6 @@ Node* insertBeforePosition(Node *last , int position , int value){
     Node *newNode = new Node;
     if(last == NULL){
         cout<<"You cannot insert at this position"<<endl;
-        // last = newNode;
-        // newNode->next = newNode;
-        // newNode->data = value;
-        // return last;
     }
     else{
     Node *p = last;
@@ -111,14 +124,16 @@ Node* insertBeforePosition(Node *last , int position , int value){
 
 // deleting
 void deleteFromFirst(Node* last){
+    if(last==NULL) cout<<"Linkedlist is already empty"<<endl;
+    return;
     Node *temp = last->next;
     Node *newFirst = last->next->next;
     last->next = newFirst;
     delete temp;
 }
-
-
 Node* deleteFromLast(Node *last){
+    if(last==NULL) cout<<"Linkedlist is already empty"<<endl;
+    return last;
     Node *p,*q;
     p = last->next;
     q = last;
@@ -129,17 +144,6 @@ Node* deleteFromLast(Node *last){
     delete q;
     return p;
 
-}
-int countList(Node *last){
-    int i = 1; //circular k case main pehle lelengay 1
-    Node *p = last->next;
-
-    while(p->next!=last->next){
-        p = p->next;
-        i++;
-    }
-
-    return i;
 }
 void deleteAtPosition(Node *last,int position){
     if(position<=0 && position>=countList(last)){
@@ -159,7 +163,6 @@ void deleteAtPosition(Node *last,int position){
 
 
 }
-
 Node* deleteWithKey(Node *last , int key){
     if(last->data == key){
         // delete at end logic;
@@ -178,13 +181,30 @@ Node* deleteWithKey(Node *last , int key){
         Node *p = last ->next->next;
         last->next = p;
         delete(q);
-        return p;
+        return last;
     }
+    else {
+        // delete at other searching place
+        Node *p = last->next;
+        Node *q = last->next->next;
+        while(q->data!=key){
+            p = p->next;
+            q = q->next;
+        }
+        p->next = q->next;
+        delete(q);
+        return last;
     
-    // delete at oter searching place
-    Node
+    }
+
+    cout<<"Key was not found!"<<endl;
+    return last;
     
-    
+}
+
+
+// Reversal of linked list
+Node* reverse(Node *last){
 
 }
 
@@ -219,59 +239,154 @@ void twoWaysTraversal(Node *last){
         current = current->next;
     } while (current!=last->next);
 
-    // last = reverse(last);
-    //  *current = last->next;
-    // do
-    // {
-    //     cout<<current->next<<endl;
-    //     current = current->next;
-    // } while (current!=last->next);
+    last = reverse(last);
+     *current = last->next;
+    do
+    {
+        cout<<current->next<<endl;
+        current = current->next;
+    } while (current!=last->next);
 
     
 }
 
-// reverse Circular linkedlist
-// Node* reverse(Node *last){
-//     Node *p,*q ,*r;
-//     p = last->next;
-//     q = last->next->next;
-//     r = last->next->next->next;
-//     p->next = last;
-//     while(r->next!=last){
-//         q->next = p;
-//         r->next = q;
-//     }
-//     return p;
-
-// }
 
 int main(){
-Node *last = NULL;    
-// last = insertAtEnd(last,12); //if condition main jayega  yeh
-// last = insertAtEnd(last,14); //yahan se else 
-// last = insertAtEnd(last,16);
-// last = insertAtEnd(last,18);
-// last = insertAtStart(last,8);
-// last = insertAtEnd(last,77);
-// last = insertAtStart(last,1);
-// insertAtPosition(last,3,55);
-// insertAfterPosition(last,2,66);
-// insertAfterPosition(last,4,66);
-// insertBeforePosition(last,3,22);
+    Node *last = NULL;
+    while (true)
+    {
+        int option;
+        int value; 
+        int key; 
+        int position;
+        cout<<"Enter Option Number to perform Action "<<endl;
+        cout<<"1 - insert at End , 2 - insert at start , 3 - insert at position , 4 - insert before positon , 5 - insert After position"<<endl;
+        cout<<"6 - delete at End , 7 - delete at start , 8 - delete at position , 9 - delete with key "<<endl;
+        cout<<"Enter 10 for traversal "<<endl;
+        cin>>option;
 
+        switch (option)
+        {
+            
+        case 1:{
+            cout<<"Enter the value ";
+            cin>>value; 
+            last = insertAtEnd(last,value);
+            break;
+        } 
+        case 2:{
+            cout<<"Enter the value ";
+            cin>>value; 
+            last = insertAtStart(last,value);
+            break;
 
-last = insertAtEnd(last,66);
-last = insertAtEnd(last,77);
-last = insertAtEnd(last,88);
-last = insertAtEnd(last,99);
-last = insertAtEnd(last,101);
+        } 
+        case 3:{
+            cout<<"Enter the value ";
+            cin>>value;
+            cout<<"Enter the position ";
+            cin>>position; 
+            insertAtPosition(last,position,value);
+            break;
 
-// cout<<countList(last)<<endl;
-deleteAtPosition(last, 3);
-normalTraversal(last);
-// deleteFromFirst(last);
-// last = deleteFromLast(last);
-// normalTraversal(last);
-// twoWaysTraversal(last);
-return 0;
+        } 
+        case 4:{
+            cout<<"Enter the value ";
+            cin>>value;
+            cout<<"Enter the position ";
+            cin>>position; 
+            last = insertBeforePosition(last,position,value);
+            break;
+        } 
+        case 5: {
+            cout<<"Enter the value ";
+            cin>>value;
+            cout<<"Enter the position ";
+            cin>>position; 
+            insertAfterPosition(last,position,value);
+            break;
+        }
+
+        case 6:{
+            deleteFromFirst(last);
+            cout<<"The first element is deleted "<<endl;
+            break;
+        } 
+
+        case 7:{
+            last = deleteFromLast(last);
+            cout<<"The last element has been deleted "<<endl;
+            break;
+        } 
+        case 8:{
+            cout<<"Enter Position"<<endl;
+            cin>>position;
+            deleteAtPosition(last,position);
+            break;
+        } 
+        case 9:{
+            cout<<"Enter key"<<endl;
+            cin>>key;
+            last = deleteWithKey(last,key);
+            break;
+        } 
+        case 10:{
+            normalTraversal(last);
+            break;
+        } 
+
+    }
+    
 }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OLD main for testing 
+
+// int main(){
+// Node *last = NULL;    
+// // last = insertAtEnd(last,12); //if condition main jayega  yeh
+// // last = insertAtEnd(last,14); //yahan se else 
+// // last = insertAtEnd(last,16);
+// // last = insertAtEnd(last,18);
+// // last = insertAtStart(last,8);
+// // last = insertAtEnd(last,77);
+// // last = insertAtStart(last,1);
+// // insertAtPosition(last,3,55);
+// // insertAfterPosition(last,2,66);
+// // insertAfterPosition(last,4,66);
+// // insertBeforePosition(last,3,22);
+
+
+// last = insertAtEnd(last,66);
+// last = insertAtEnd(last,77);
+// last = insertAtEnd(last,88);
+// last = insertAtEnd(last,99);
+// last = insertAtEnd(last,101);
+
+// // cout<<countList(last)<<endl;
+// // deleteAtPosition(last, 3);
+// last = deleteWithKey(last,88);
+// normalTraversal(last);
+// // deleteFromFirst(last);
+// // last = deleteFromLast(last);
+// // normalTraversal(last);
+// // twoWaysTraversal(last);
+// return 0;
+// }
