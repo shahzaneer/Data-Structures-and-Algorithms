eba860c04d03d4f679fe5212adb069c192b44784
Shahzaneer Ahmed <shahism01@gmail.com>
2022-04-05 01:57:11 +0500

    queue md + queue with array Added 💫

---
+++ Codes/DS and Algo/Queue/QueueWithArray.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+struct Queue{
+    int size;
+    int f = -1;
+    int r = -1;
+    int *arr;
+};
+
+Queue *q = NULL;
+
+int isFull(){
+    if(q->r == q->size-1){
+        return 1;
+    }
+    return 0;
+}
+
+int isEmpty(){
+    if(q->f == q->r){
+        // ager -1 -1 hon tou bhi aur ager deque krte krte f aur b equal hojayen tb bhi!
+        q->f = q->r = -1;
+        return 1;
+    }
+    return 0;
+}
+
+void enqueue(int value){
+    if(isFull()){
+        cout<<"Enqueue is not possible\n";
+        return;
+    }
+//  logic for enqueue 
+    q->r++;
+    q->arr[q->r] = value;
+
+
+}
+int dequeue(){
+    if(isEmpty()){
+        cout<<"Queue is already Empty \n";
+        return -100; // -100 means there is no dequeued value!
+    }
+    // logic for dequeue
+    q->f++;
+    int dequeuedElement = q->arr[q->f];
+    return dequeuedElement;
+
+}
+
+void display(){
+    if(isEmpty()){
+        cout<<"The Queue is Empty !!"<<endl;
+        return;
+    }
+    for(int i=q->f+1;i<=q->r;i++){
+        cout<<q->arr[i]<<endl;
+    }
+}
+
+int main(){
+    q = new Queue; //DMA things
+    int size;
+    cout<<"Enter the size of queue\n";
+    cin>>size;
+    q->size = size; 
+    q->arr = new int [q->size]; 
+    int option; 
+
+    do{
+        cout<<"Enter options to perform corresponding actions \n";
+        cout<<"1 - Enqueue\n"
+            <<"2 - DeQueue\n"
+            <<"3 - Display\n"
+            <<"-1 - Exit\n";
+            
+        cin>>option;
+        switch (option)
+        {
+        case 1:
+        {
+            int val;
+            cout<<"Enter value \n";
+            cin>>val;
+            enqueue(val);
+            break;
+
+        }
+
+        case 2:
+        {
+            dequeue();
+            break;
+        }
+
+        case 3:
+        {
+            display();
+            break;
+        }
+        case -1:
+        {
+            cout<<"exiting . . . \n";
+            break;
+        }
+        }    
+    }while(option!=-1);
+
+    return 0;
+}
---
+++ Codes/DS and Algo/Queue/queue.md
@@ -0,0 +1,12 @@
+# Queue
+- Linear Data Structure
+- it follows FIFO Sequence
+- FIFO -> First in first out manner
+- It can be implemented as Arrays , LinkedList and stack as well.
+
+# Important Methods
+- enqueue (new element queue main lagana)
+- dequeue (queue k start se aik element delete krna)
+- Peek (position of some element)
+- isEmpty (boolean)
+- isFull (boolean)
