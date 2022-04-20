2092477f873dbace04f5a605fe4a310c2e4079f9
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-04-20 06:35:49 +0500

    stack assignment update 1.0

--- Lab Task Code/DSA Lab Task -3/palindromedoublylinkedlist.cpp
+++ Lab Task Code/DSA Lab Task -3/palindromedoublylinkedlist.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
+struct Node
+{
     char data;
     Node *next;
     Node *previous;
@@ -9,16 +10,20 @@ struct Node{
 
 Node *top = NULL;
 
-int isEmpty(){
-    if(top == NULL){
+int isEmpty()
+{
+    if (top == NULL)
+    {
         return 1;
     }
     return 0;
 }
 
-int isFull(){
-    Node *p = new Node; //heap main memory request kro
-    if(p == NULL){
+int isFull()
+{
+    Node *p = new Node; // heap main memory request kro
+    if (p == NULL)
+    {
         // ager na ho tou mtlb stack full hogya hai
 
         return 1;
@@ -26,24 +31,27 @@ int isFull(){
     return 0;
 }
 
-Node *push(char value){
-    if(isFull() == 1){
-        cout<<"Stack overflow !\n";
+Node *push(char value)
+{
+    if (isFull() == 1)
+    {
+        cout << "Stack overflow !\n";
         return top;
-
     }
 
     Node *newNode = new Node;
     newNode->data = value;
 
-    if(isEmpty()== 1){
+    if (isEmpty() == 1)
+    {
         top = newNode;
         top->previous = NULL;
         top->next = NULL;
         return top;
     }
-    else{
-        Node* newt = top;
+    else
+    {
+        Node *newt = top;
         newNode->previous = NULL;
         newNode->next = newt;
         top = newNode;
@@ -51,12 +59,15 @@ Node *push(char value){
     }
 }
 
-char pop(){
-    if(isEmpty()==1){
-        cout<<"The stack is already Empty \n";
-        return' ';
+char pop()
+{
+    if (isEmpty() == 1)
+    {
+        cout << "The stack is already Empty \n";
+        return ' ';
     }
-    if(top->next == NULL){
+    if (top->next == NULL)
+    {
         // cout<<"The popped Value is :"<<top->data<<endl;
         char value;
         value = top->data;
@@ -64,44 +75,78 @@ char pop(){
         return value;
     }
     // lifo k mutabiq pop bhi start main se hi krna hai.
-    else if(top->next!=NULL){
-    Node *p = top;
-    top = top->next;
-    top->previous = NULL;
-    char poppedValue = p->data;
-    p->next = NULL;
-    p->previous = NULL;
-    p = NULL;
-    delete(p);
-    // cout<<"The popped value is :"<<poppedValue<<endl;
-    return poppedValue;
+    else if (top->next != NULL)
+    {
+        Node *p = top;
+        top = top->next;
+        top->previous = NULL;
+        char poppedValue = p->data;
+        p->next = NULL;
+        p->previous = NULL;
+        p = NULL;
+        delete (p);
+        // cout<<"The popped value is :"<<poppedValue<<endl;
+        return poppedValue;
     }
-    
+
     return ' ';
 }
 
-int isPalindrome(string str){
-    for(int i=0;i<str.length();i++){
+int isPalindrome(string str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
         top = push(str[i]);
     }
-    for(int i = 0;i<str.length();i++){
-        if(pop() != str[i]){
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (pop() != str[i])
+        {
             return 0;
         }
     }
     return 1;
 }
 
-
-int main(){
-    cout<<"Enter you String \n";
-    string str;
-    cin>>str;
-    if(isPalindrome(str) == 1){
-        cout<<"Its a palindrome !"<<endl;
+int isPalindromeSecondWay(string str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        top = push(str[i]);
     }
-    else{
-        cout<<"Its not a Palindrome !"<<endl;
+
+    Node *last = top;
+    while (last->next != NULL)
+    {
+        last = last->next;
+    }
+
+    
+        while (top != last->previous || top != last)
+        {
+            if (top->data != last->data)
+            {
+                return 0;
+            }
+            top = top->next;
+            last = last->previous;
+        }
+
+    return 1;
+}
+
+int main()
+{
+    cout << "Enter you String \n";
+    string str;
+    cin >> str;
+    if (isPalindromeSecondWay(str) == 1)
+    {
+        cout << "Its a palindrome !" << endl;
+    }
+    else
+    {
+        cout << "Its not a Palindrome !" << endl;
     }
 
     return 0;
