58b988755bf924fb262b378ae1123209e0865a6d
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-04-26 02:08:12 +0500

    infix to postfix final update 💥

--- Lab Task Code/DSA Lab Task -3/infixToPostfix.cpp
+++ Lab Task Code/DSA Lab Task -3/infixToPostfix.cpp
@@ -2,128 +2,189 @@
 #include <string.h>
 using namespace std;
 
+//* infix to Postfix Conversion can be done with any other logic but for this purpose we need to scan the
+//* Statement Multiple Times , But With Stack We can Do this thing with only one Scan
 
-struct stack{
+struct stack
+{
     int size;
-    int top = -1; 
-    char *arr; 
-    
+    int top = -1;
+    char *arr;
 };
 
-int isEmpty(stack *s){
+int isEmpty(stack *s)
+{
     // boolean function hai using primitive int in form of 0 and 1 -> the motivation for boolean data type.
-    if(s->top<0){
+    if (s->top < 0)
+    {
         return 1;
         // 1 means  True
     }
-    else{
+    else
+    {
         return 0;
         // 0 means False
     }
 }
-int isFull(stack *s){
+int isFull(stack *s)
+{
 
-    if(s->top == s->size-1){
-    
+    if (s->top == s->size - 1)
+    {
+
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
-
 }
 
-void push(stack *s , char value){
-    if(isFull(s) == 1){
-        cout<<"The Stack is already filled"<<endl;
+void push(stack *s, char value)
+{
+    if (isFull(s) == 1)
+    {
+        cout << "The Stack is already filled" << endl;
         return;
     }
-    else{
-    s->top++;
-    s->arr[s->top] = value;
+    else
+    {
+        s->top++;
+        s->arr[s->top] = value;
     }
 }
-char pop(stack *s){
-    if(isEmpty(s) == 1){
-        cout<<"The stack is already Empty"<<endl;
-        return ' '; // NULL in the case of empty stack 
+char pop(stack *s)
+{
+    if (isEmpty(s) == 1)
+    {
+        cout << "The stack is already Empty" << endl;
+        return ' '; // NULL in the case of empty stack
     }
-    
-    else{
-    char poppedValue = s->arr[s->top];
-    s->top--;
-    return poppedValue;
+
+    else
+    {
+        char poppedValue = s->arr[s->top];
+        s->top--;
+        return poppedValue;
     }
 }
-char peek(stack *s){
+char peek(stack *s)
+{
     char peekElement = s->arr[s->top];
     return peekElement;
 }
-void display(char* arr){
-    int i = 0;
-    while(arr[i]!= '\0'){
-        cout<<arr[i];
-        i++;
-    }
-    cout<<endl;
-}
 
-int isOperator(char c){
-    if(c=='+' || c == '-' || c=='/' || c=='*'){
+int isOperator(char c)
+{
+    if (c == '+' || c == '-' || c == '/' || c == '*' || c == '^' || c == '(' || c == ')')
+    {
         return 1;
     }
     return 0;
 }
 
-int precedence(char c){
-    if(c == '*' || c== '/'){
+int precedence(char c)
+{
+    if (c == '^')
+        return 3;
+    else if (c == '*' || c == '/')
+    
         return 2;
-    }
-    if(c == '-' || c == '+'){
+    
+    else if (c == '-' || c == '+')
+    
         return 1;
-    }
-    return 0;
+
+    else
+        return 0;
 }
-char* infixToPostfix(string expression , stack *s){
+void infixToPostfix(string expression, stack *s)
+{
+
+    string postfix;
+
+    for (int i = 0; i < expression.length(); i++)
+    {
+        // Traversing the whole Expression one time !
+
+        if (!isOperator(expression[i]) == 1)
+        {
+            // If you encounter operands simply add them to the postfix Statement!
+            postfix += expression[i];
+        }
+
+        // Special case for Brackets
+
+        else if (expression[i] == '(')
+        {
+            push(s, expression[i]);
+        }
+
+        else if (expression[i] == ')')
+        {
+            while (!isEmpty(s) && peek(s) != '(')
+            {
+                postfix += peek(s);
+                pop(s);
+            }
+            pop(s); // so that '(' bhi pop ho jaye!
+        }
+
+        else
+        {
+            // else koi na koi operator hoga
+            // ab ismay teen conditions hain
+            // aik ager stack already empty ho (push)
+            // second ager precedence current char ki zyada ho peek char se (push)
+            // ager precedence current char ki kam ho peek char se (pop)
+            if (isEmpty(s))
+            {
+                push(s, expression[i]);
+            }
+
+            else if (precedence(expression[i]) > precedence(peek(s)))
+            {
+                push(s, expression[i]);
+            }
+            else
+            {
+                while (!isEmpty(s) && precedence(expression[i]) <= precedence(peek(s)))
+                {
+                    postfix += peek(s);
+                    pop(s);
+                
+                }
+                push(s, expression[i]); //jese hi precedence greater ho jaye push it to the stack
+            }
+        }
+
+        
+    }
+
+    // Now after the traversal, adding the operators present in the stack to the postfix Expression
+
+    while (!isEmpty(s))
+    {
+        postfix += peek(s);
+        pop(s);
+    }
+    // stack main mojud characters ko postfix main daalna hai!
+
+    cout << postfix << endl;
     
-    char *postfix = new char [expression.length()+1];
-    int j = 0;//for postfix
-    for(int i = 0; i<expression.length();i++){
-        if(!isOperator(expression[i])){
-            postfix[j] = expression[i];
-            j++;
-        }
-        else{
-            if(precedence(expression[i])>precedence(peek(s))){
-                push(s,expression[i]);
-            }
-            else{
-                char poppedCharacter = pop(s);
-                postfix[j] = poppedCharacter;
-                j++;
-            }
-        }
-    }
-// stack main mojud characters ko postfix main daalna hai!
-    while(!isEmpty(s)){
-        postfix[j] = pop(s);
-        j++;
-    }
-
-
-    return postfix;
 }
 
-int main(){
-    // Making Stack 
+int main()
+{
+    // Making Stack
     stack *s = new stack;
-    s->size = 200; 
-    s->arr  = new char [s->size];
-    cout<<"Enter you Expression \n";
+    s->size = 200;
+    s->arr = new char[s->size];
+    cout << "Enter you Expression \n";
     string expression;
-    cin>>expression;
-    char *postfix = infixToPostfix(expression,s);
-    display(postfix);
+    cin >> expression;
+    infixToPostfix(expression, s);
     
+
     return 0;
 }
