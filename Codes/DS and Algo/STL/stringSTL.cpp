3fa6212daa59bea730d293016fe62dd475dfa147
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-06-08 02:53:17 +0500

    string STL added

---
+++ Codes/DS and Algo/STL/stringSTL.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// In C Language we use String as a character array in which the last element is the null character
+// also we have to specify the size of static character array
+
+// ! array implementations cannot show concatenation , assignment, copy assignment operator
+
+
+int main(){
+    string s = "Shahzaneer Ahmed";
+    string s = string("Jalaluddin Muhammad");
+
+
+    return 0;
+}
