264f999f419160da6e0104594463ade570fad82b
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-06-08 02:57:49 +0500

    stack STL added

--- Codes/DS and Algo/STL/Stack.cpp
+++ Codes/DS and Algo/STL/Stack.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <stack>
+
+// Stack has been discussed properly in the ADT SECTION
 using namespace std;
 int main(){
-stack <int> s;
-s.push(5);
-s.push(10);
-s.push(15);
-s.push(20);
- int n;
-for(int i=0; i<=s.size()+1;i++){
-     n = s.empty();
-     s.pop();
-    cout<<n<<endl;
-}
-return 0;
+    stack <int> s;
+    s.push(5);
+    s.push(10);
+    s.push(15);
+    s.push(20);
+
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    stack<string> s2;
+    s2.push("Noor");
+    s2.push("Ali");
+    s2.push("Hassan");
+    s2.push("Ahmed");
+
+    while (!s2.empty())
+    {
+        cout << s2.top() << " ";
+        s2.pop();
+    }
+
+    return 0;
 }
