9850f36064386bb0561011eb5a79b8bdab218484
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-06-15 14:13:57 +0500

    Default argument added

--- Codes/DS and Algo/Searching Algorithms/BinarySearchRecursive.cpp
+++ Codes/DS and Algo/Searching Algorithms/BinarySearchRecursive.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr [] , int size , int low , int high , int key){
+int binarySearch(int arr [] , int size , int low , int high , int key = 5){
     int mid = low + ((high - low) / 2);
 
     if (low > high)
@@ -25,7 +25,7 @@ int binarySearch(int arr [] , int size , int low , int high , int key){
 
 int main(){
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int elementFoundAtIndex = binarySearch(arr,9,0,9-1,10);
+    int elementFoundAtIndex = binarySearch(arr,9,0,9-1);
     // cout << elementFoundAtIndex << endl;
 
     if (elementFoundAtIndex == -1)
